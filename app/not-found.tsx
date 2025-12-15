15b6cfc37723e468afc94561f39032219612b586
webxrahat <islammdrahat308@gmail.com>
2025-12-15 12:53:26 +0600

    project structure

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function notfound() {
-  return <div>notfound</div>;
+  return <div>Root not found</div>;
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <div>
+    <div className="bg-lime-400 space-x-2">
+      <Link href={"/"}>Home</Link>
       <Link href={"/myblog"}>Blog</Link>
       <Link href={"/products"}>Products</Link>
+      <Link href={"/dashboard"}>Dashboard</Link>
     </div>
   );
 }
